effbc21910d1daa6029ff6c6454c0089802fed54
mo khan <mo@mokhan.ca>
2009-05-08 09:21:59 -0600

    renamed Thread to CurrentThread and implemented lookup for thread local storage

--- product/Gorilla.Commons.Infrastructure/Transactions/IThread.cs
+++ product/Gorilla.Commons.Infrastructure/Transactions/IThread.cs
@@ -2,6 +2,6 @@ namespace Gorilla.Commons.Infrastructure.Transactions
 {
     public interface IThread
     {
-        T provide_slot_for<T>() where T : new();
+        T provide_slot_for<T>() where T : class, new();
     }
 }
